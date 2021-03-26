7b80713e07359c524153ee57675d5c0ec1eb9e7f
Zacandri <andrea.zacconi2@studio.unibo.it>
2021-03-26 14:31:36 +0100

    Fixed warnings in Exit

--- src/main/java/view/Exit.java
+++ src/main/java/view/Exit.java
@@ -4,22 +4,31 @@ import java.awt.Dimension;
 import java.awt.Toolkit;
 import constants.ViewConstants;
 
-import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.scene.layout.VBox;
 
-public class Exit {
+/**
+ * Class that implements the confirm box EXIT.
+ * */
+public final class Exit {
 
     private static boolean answer;
 
+    private Exit() {
+        /**Not called.*/
+    }
+
+    /**
+     * Creation of the confirm box.
+     * @param title String 
+     * @param message String
+     * @return answer boolean
+     * */
     public static boolean display(final String title, final String message) {
 
         Stage window = new Stage();
@@ -39,15 +48,15 @@ public class Exit {
         label.setStyle("    -fx-text-fill: #FFFFFF;\r\n"
                 + "    -fx-background-radius: 6;\r\n"
                 + "    -fx-font-weight: bold;\r\n"
-                + "    -fx-background-color: rgba(0, 0, 0, 0.5);\r\n"
-                +"      -fx-font-size:"+sw/150+";");
+                + "     -fx-background-color: rgba(0, 0, 0, 0.5);\r\n"
+                + "      -fx-font-size:" + sw / ViewConstants.DIVISOR_150 + ";");
 
         Button yesButton = new Button("YES");
         yesButton.setStyle(" -fx-background-radius: 6; "
                 + "-fx-font-weight: bold;\r\n"
                 + "-fx-text-fill: #FFFFFF;\r\n"
                 + "-fx-background-color: linear-gradient(#000000, #696969);\r\n"
-                +" -fx-font-size:"+sw/150+";");
+                + " -fx-font-size:" + sw / ViewConstants.DIVISOR_150 + ";");
         yesButton.setPrefSize(sw / ViewConstants.DIVISOR_25, sh / ViewConstants.DIVISOR_25);
 
         Button noButton = new Button("NO");
@@ -55,8 +64,8 @@ public class Exit {
                 + "-fx-font-weight: bold;\r\n"
                 + "-fx-text-fill: #FFFFFF;\r\n"
                 + "-fx-background-color: linear-gradient(#000000, #696969);\r\n"
-                +" -fx-font-size:"+sw/150+";");
-        
+                + " -fx-font-size:" + sw / ViewConstants.DIVISOR_150 + ";");
+
         noButton.setPrefSize(sw / ViewConstants.DIVISOR_25, sh / ViewConstants.DIVISOR_25);
 
         yesButton.setOnAction(e -> {
