b161ba326be77c36cc89c1c999fcbc197a8e610b
Giosuè Mainardi <giosue.mainardi@studio.unibo.it>
2021-03-27 15:16:38 +0100

    Updated build file to the new version and added minor updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,23 @@
-// Declaration of the Gradle extension to use
 plugins {
+    // Apply the java plugin to add support for Java
     java
+
+    // Apply the application plugin to add support for building a CLI application
+    // You can run your app via task "run": ./gradlew run
     application
+
     /*
      * Adds tasks to export a runnable jar.
      * In order to create it, launch the "shadowJar" task.
      * The runnable jar will be found in build/libs/projectname-all.jar
      */
-    id("com.github.johnrengelman.shadow") version "5.2.0"
+    id("com.github.johnrengelman.shadow") version "6.1.0"
 }
+
 repositories {
-    jcenter() // Contains the whole Maven Central + other stuff
+    mavenCentral()
 }
-// List of JavaFX modules you need. Comment out things you are not using.
+
 val javaFXModules = listOf(
     "base",
     "controls",
@@ -20,28 +25,54 @@ val javaFXModules = listOf(
     "swing",
     "graphics"
 )
-// All required for OOP
-val supportedPlatforms = listOf("linux", "mac", "win")
+
+val supportedPlatforms = listOf("linux", "mac", "win") // All required for OOP
+
+val javaFxVersion = "15.0.1"
+
+val jUnitVersion = "5.7.1"
 
 dependencies {
     // Example library: Guava. Add what you need (and remove Guava if you don't use it)
-    implementation("com.google.guava:guava:28.1-jre")
+     implementation("com.google.guava:guava:28.1-jre")
+
     // JavaFX: comment out if you do not need them
     for (platform in supportedPlatforms) {
         for (module in javaFXModules) {
-            implementation("org.openjfx:javafx-$module:13:$platform")
+            implementation("org.openjfx:javafx-$module:$javaFxVersion:$platform")
         }
     }
+
     // JUnit API and testing engine
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.5.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$jUnitVersion")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$jUnitVersion")
 }
 
-tasks.withType<Test> {
-    // Enables JUnit 5 Jupiter module
-    useJUnitPlatform()
+tasks {
+    withType<JavaCompile> {
+        options.encoding = "UTF-8"
+    }
+
+    withType<Test> {
+        // Enables JUnit 5 Jupiter module
+        useJUnitPlatform()
+    }
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
 }
 
 application {
-    mainClassName = "application.Launcher"
+    // Define the main class for the application
+    mainClass.set("application.Laucher")
+
+    /*
+     * mainClassName was deprecated by Gradle, but it is still required by John Engelman's Shadow plugin.
+     * A pull request with a fix was already merged, but it hasn't been released yet;
+     * see https://github.com/johnrengelman/shadow/issues/609 and https://github.com/johnrengelman/shadow/pull/612
+     */
+    @Suppress("DEPRECATION")
+    mainClassName = mainClass.get()
 }
