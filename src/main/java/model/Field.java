bf72179cc3f3748111f57c05d8bd47ce51663176
Giosuè Mainardi <giosue.mainardi@studio.unibo.it>
2021-03-29 14:42:33 +0200

    Added methods javadoc

--- src/main/java/model/Field.java
+++ src/main/java/model/Field.java
@@ -8,12 +8,42 @@ import utilities.Pair;
 
 public interface Field {
 
+    /**
+     * @param laneIndex the lane in wich insert the unit
+     * @param unit the unit to be inserted
+     * @throws throws an Exception if the laneIndex doesn't exist
+     */
     void addUnit(int laneIndex, Unit unit);
+
+    /**
+     * @return a list of the lanes of the field
+     */
     List<Lane> getLanes();
+
+    /**
+     * @param player the player whose score to get
+     * @return the numbers of units that have received the enemy base, if there are any
+     */
     Optional<Integer> getScore(PlayerType player);
+
+    /**
+     * @return a map of the units with their corresponding position, rappresented by a pair (y= lane number)
+     */
     Map<Unit, Pair<Integer, Integer>> getUnits();
+
+    /**
+     * Update all the lanes.
+     */
     void update();
+
+    /**
+     * @return the number of lanes created
+     */
     int getLaneNumber();
+
+    /**
+     * @return the leght of each lane
+     */
     int getCellsNumber();
 
 }
--- src/main/java/model/Lane.java
+++ src/main/java/model/Lane.java
@@ -5,10 +5,32 @@ import java.util.Set;
 
 public interface Lane {
 
-    void addUnit(Unit u);
+    /**
+     * @param unit unit to be added
+     */
+    void addUnit(Unit unit);
+
+    /**
+     * @param position 
+     * @return the set of the units in this position
+     * @throws IllegalArgumentException in case of the position is out of the lane
+     */
     Set<Unit> getUnitsAtPosition(int position);
+
+    /**
+     * @return a map of unit with the corresponding position in this lane
+     */
     Map<Unit, Integer> getUnits();
+
+    /**
+     * @return the lenght of the lane
+     */
     int getLenght();
+
+    /**
+     * @param player the player whose score to get
+     * @return the numbers of units that have received the enemy base by this lane, if there are any
+     */
     Integer getScore(PlayerType player);
 
     /**
