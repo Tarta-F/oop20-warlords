e4aa77889f860df330387025eb0d17c675efc346
Giosuè Mainardi <giosue.mainardi@studio.unibo.it>
2021-04-09 13:05:51 +0200

    Spawn Unit method shows in the view the units, converter from M to V

--- src/main/java/controllers/ControllerImpl.java
+++ src/main/java/controllers/ControllerImpl.java
@@ -1,15 +1,18 @@
 package controllers;
-//import org.apache.commons.lang3.tuple.Pair;
 
-import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.lang3.tuple.Pair;
 
 import constants.GameConstants;
-import javafx.scene.layout.Pane;
 import model.FieldImpl;
 import model.PlayerType;
+import model.Unit;
 import model.UnitImpl;
 import model.UnitType;
 import view.GameView;
+import view.UnitViewType;
 
 public final class ControllerImpl implements Controller {
     //private Pair<Pair<PlayerType, Integer>, Pair<PlayerType, Integer>> selectedLaneIndex;
@@ -20,8 +23,8 @@ public final class ControllerImpl implements Controller {
     private int selectedUnitIndexP2 = 0;
     private long lastSpawnP1 = 0;
     private long lastSpawnP2 = 0;
-    private GameView gameView;
-    private FieldImpl field;
+    private final GameView gameView;
+    private final FieldImpl field;
     //private Pane pane;
     //TODO timer dei player da mostrare alla view ->
 
@@ -42,11 +45,30 @@ public final class ControllerImpl implements Controller {
         new Thread(new GameTimer(1, this.gameView)).start();
     }
 
+    private UnitViewType convertUnit(final Unit modelUnit) {
+        final UnitType modelType = modelUnit.getUnitType();
+        final PlayerType player = modelUnit.getPlayer();
+        switch (modelType) {
+            case SWORDSMEN:
+                return player.equals(PlayerType.PLAYER1) ? UnitViewType.SWORDSMEN_PLAYER1 : UnitViewType.SWORDSMEN_PLAYER2;
+            case SPEARMEN:
+                return player.equals(PlayerType.PLAYER1) ? UnitViewType.SPEARMEN_PLAYER1 : UnitViewType.SPEARMEN_PLAYER2;
+            case ARCHER:
+                return player.equals(PlayerType.PLAYER1) ? UnitViewType.ARCHER_PLAYER1 : UnitViewType.ARCHER_PLAYER2;
+            default:
+                return null;
+        }
+    }
+    private Map<UnitViewType, Pair<Integer, Integer>> convertMap(final Map<Unit, Pair<Integer, Integer>> modelMap) {
+        final Map<UnitViewType, Pair<Integer, Integer>> viewMap = new HashMap<>();
+        modelMap.forEach((k, v) -> viewMap.put(this.convertUnit(k), v));
+        return viewMap;
+    }
+
     @Override
     public void controlNextLane(final PlayerType playerType) {
         final int currentIndex = playerType.equals(PlayerType.PLAYER1) ? this.selectedLaneIndexP1 : this.selectedLaneIndexP2;
         final int nextIndex = (currentIndex + 1) % GameConstants.FIVE_LANES;
-        //this.selectedLaneIndexP1 = nextIndex;
         if (playerType.equals(PlayerType.PLAYER1)) {
             this.selectedLaneIndexP1 = nextIndex;
         }
@@ -65,7 +87,6 @@ public final class ControllerImpl implements Controller {
         } else {
             nextIndex = (currentIndex - 1) % GameConstants.FIVE_LANES;
         }
-        //this.selectedLaneIndexP1 = nextIndex;
         if (playerType.equals(PlayerType.PLAYER1)) {
             this.selectedLaneIndexP1 = nextIndex;
         }
@@ -84,7 +105,7 @@ public final class ControllerImpl implements Controller {
         if (timeWaited > unitToSpawn.getTimer()) { //probabilmente da castare a long (per correttezza (?))
             final int lane = playerType.equals(PlayerType.PLAYER1) ? this.selectedLaneIndexP1 : this.selectedLaneIndexP2;
             this.field.addUnit(lane, new UnitImpl(unitToSpawn, playerType));
-          //gameView.show(Mappa con unità da stampare);  (?)
+            gameView.show(this.convertMap(this.field.getUnits()));
         }
     }
 
