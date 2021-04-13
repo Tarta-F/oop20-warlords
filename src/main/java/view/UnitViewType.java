7bcbdc38821f4412b56746bde4a528fcb3ac890d
Giosuè Mainardi <giosue.mainardi@studio.unibo.it>
2021-04-13 21:24:59 +0200

    Now each Type have his waitingTime

--- src/main/java/view/UnitViewType.java
+++ src/main/java/view/UnitViewType.java
@@ -1,5 +1,8 @@
 package view;
 
+import java.util.concurrent.TimeUnit;
+
+import constants.GameConstants;
 import constants.ViewImages;
 
 /**
@@ -8,28 +11,35 @@ import constants.ViewImages;
 public enum UnitViewType {
 
     /**Swordsmen of player 1.*/
-    SWORDSMEN_PLAYER1(ViewImages.SEP + "SwordsmenP1.png"),
+    SWORDSMEN_PLAYER1(ViewImages.SEP + "SwordsmenP1.png", TimeUnit.MILLISECONDS.toSeconds(GameConstants.SWORDSMEN_TIMER)),
     /**Swordsmen of player 2.*/
-    SWORDSMEN_PLAYER2(ViewImages.SEP + "SwordsmenP2.png"),
+    SWORDSMEN_PLAYER2(ViewImages.SEP + "SwordsmenP2.png", TimeUnit.MILLISECONDS.toSeconds(GameConstants.SWORDSMEN_TIMER)),
 
     /**Spearmen of player 1.*/
-    SPEARMEN_PLAYER1(ViewImages.SEP + "SpearmenP1.png"),
+    SPEARMEN_PLAYER1(ViewImages.SEP + "SpearmenP1.png", TimeUnit.MILLISECONDS.toSeconds(GameConstants.SPEARMEN_TIMER)),
     /**Spearmen of player 2.*/
-    SPEARMEN_PLAYER2(ViewImages.SEP + "SpearmenP2.png"),
+    SPEARMEN_PLAYER2(ViewImages.SEP + "SpearmenP2.png", TimeUnit.MILLISECONDS.toSeconds(GameConstants.SPEARMEN_TIMER)),
 
     /**Archer of player 1.*/
-    ARCHER_PLAYER1(ViewImages.SEP + "ArcherP1.png"),
+    ARCHER_PLAYER1(ViewImages.SEP + "ArcherP1.png", TimeUnit.MILLISECONDS.toSeconds(GameConstants.ARCHER_TIMER)),
     /**Archer of player 2.*/
-    ARCHER_PLAYER2(ViewImages.SEP + "ArcherP2.png");
+    ARCHER_PLAYER2(ViewImages.SEP + "ArcherP2.png", TimeUnit.MILLISECONDS.toSeconds(GameConstants.ARCHER_TIMER));
 
     private final String path;
+    private final long waitingTime;
 
-    UnitViewType(final String path) {
+    UnitViewType(final String path, final long waitingTime) {
         this.path = path;
+        this.waitingTime = waitingTime;
     }
 
     /**@return unit path. */
     public String getPath() {
         return this.path;
+    }
+
+    /**@return unit wainting time. */
+    public int getWaitingTime() {
+        return (int) this.waitingTime;
     }
 }
