dba66e868b5607c1c435d4d1d556f8b5b59446ea
Tarta-F <filippo.tartagni@studio.unibo.it>
2021-04-17 10:55:54 +0200

    player timer fix

--- src/main/java/controllers/PlayerTimer.java
+++ src/main/java/controllers/PlayerTimer.java
@@ -7,14 +7,14 @@ import view.game.GameViewImpl;
 public class PlayerTimer implements Runnable {
 
     private static final int SEC_IN_MIN = 60;
-    private volatile int mins;
+    //private volatile int mins;
     private volatile int seconds;
     private volatile int totSec;
     private final PlayerType playerType;
     private final GameViewImpl gameView;
 
     PlayerTimer(final GameViewImpl gameView, final PlayerType playerType) {
-        this.mins = 0;
+        //this.mins = 0;
         this.seconds = 0;
         this.totSec = 0;
         this.playerType = playerType;
@@ -25,10 +25,11 @@ public class PlayerTimer implements Runnable {
     public final void run() {
         while (totSec >= 0) {
             try {
+//              this.totSec++;
                 this.seconds = totSec % SEC_IN_MIN;
-                this.mins = (totSec - seconds) / SEC_IN_MIN;
-                this.totSec++;
+                //this.mins = (totSec - seconds) / SEC_IN_MIN;
                 this.gameView.updatePlayerTimer(this.totSec, this.playerType);
+                this.totSec++;
                 Thread.sleep(1000);
             } catch (InterruptedException ex) {
                 ex.printStackTrace();
@@ -38,7 +39,7 @@ public class PlayerTimer implements Runnable {
     /** Reset the timer (restart from 00:00). */
     public final void resetTimer() {
         this.totSec = 0;
-        this.mins = 0;
+        //this.mins = 0;
         this.seconds = 0;
     }
 }
