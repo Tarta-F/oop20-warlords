e6fb5d9656aeee30d3a23c3c70d65ac32c733970
g-mainardi <giosue.mainardi@studio.unibo.it>
2021-04-21 15:23:57 +0200

    Created Factory class for View Elements

---
+++ src/main/java/view/ViewFactory.java
@@ -0,0 +1,58 @@
+package view;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public final class ViewFactory {
+
+    private ViewFactory() {
+    }
+
+    public static Button createButton(final String message, final String style, final double width, final double height) {
+        final Button button = new Button(message);
+        button.setStyle(style);
+        button.setPrefSize(width, height);
+        button.setAlignment(Pos.CENTER);
+        return button;
+    }
+
+    public static Label createLabel(final String message, final String style, final double width, final double height) {
+        final Label label = new Label(message);
+        label.setStyle(style);
+        label.setPrefSize(width, height);
+        label.setAlignment(Pos.CENTER);
+        return label;
+    }
+
+    public static VBox createVBox(final double width) {
+        final VBox vBox = new VBox(width);
+        vBox.setAlignment(Pos.CENTER);
+        return vBox;
+    }
+
+    public static HBox createHBox(final double height, final double padding) {
+        final HBox hBox = createHBox(height);
+        hBox.setPadding(new Insets(padding, 0, padding, 0));
+        return hBox;
+    }
+
+    public static HBox createHBox(final double height) {
+        final HBox hBox = new HBox(height);
+        hBox.setAlignment(Pos.CENTER);
+        return hBox;
+    }
+
+    public static ToggleButton createToggleButton(final String message, final String style, final double width, final double height) {
+        final ToggleButton button = new ToggleButton(message);
+        button.setStyle(style);
+        button.setPrefSize(width, height);
+        button.setAlignment(Pos.CENTER);
+        return button;
+    }
+
+}
