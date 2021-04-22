bcb371db1b77d8afcc44ca627ededac8ef690ecd
Tarta-F <filippo.tartagni@studio.unibo.it>
2021-04-22 11:46:42 +0200

    now timer use AtomicLong

--- src/main/java/controllers/GameTimer.java
+++ src/main/java/controllers/GameTimer.java
@@ -1,13 +1,16 @@
 package controllers;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 import view.game.GameView;
 
 public class GameTimer implements Runnable {
 
     private static final int SEC_IN_MIN = 60;
+    private static final long SEC = 1000L;
     private volatile int mins;
     private volatile int seconds;
-    private volatile int totSec;
+    private volatile AtomicLong totSec;
     private volatile boolean stop;
     private final GameView gameView;
     private final Controller controller;
@@ -15,7 +18,7 @@ public class GameTimer implements Runnable {
     GameTimer(final int mins, final GameView gameView, final Controller controller) {
         this.mins = mins;
         this.seconds = 0;
-        this.totSec = this.mins * SEC_IN_MIN;
+        this.totSec = new AtomicLong(this.mins * SEC_IN_MIN);
         this.stop = false;
         this.gameView = gameView;
         this.controller = controller;
@@ -23,18 +26,18 @@ public class GameTimer implements Runnable {
 
     @Override
     public final void run() {
-        while (totSec >= 0 && !stop) {
+        while (this.totSec.get() >= 0 && !stop) {
             try {
-                this.seconds = totSec % SEC_IN_MIN;
-                this.mins = (totSec - seconds) / SEC_IN_MIN;
-                this.totSec--;  /*[warning spotbugs] but it's safe because there is only 1 thread that use this variable*/
+                this.seconds = this.totSec.intValue() % SEC_IN_MIN;
+                this.mins = (this.totSec.intValue() - seconds) / SEC_IN_MIN;
+                this.totSec.decrementAndGet();
                 this.gameView.updateTimer(this.mins, this.seconds);
-                Thread.sleep(1000);
+                Thread.sleep(SEC);
             } catch (InterruptedException ex) {
                 ex.printStackTrace();
             }
         }
-        //TODO block/finish the game Funzione che guarda punteggio: vincitore o pareggio.
+        /* Notify the controller that timer countdown is over. */
         this.controller.setTimerIsOver();
     }
 
--- src/main/java/controllers/PlayerTimer.java
+++ src/main/java/controllers/PlayerTimer.java
@@ -1,17 +1,19 @@
 package controllers;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 import constants.PlayerType;
 import view.game.GameView;
 
 public class PlayerTimer implements Runnable {
 
-    private volatile int totSec;
+    private volatile AtomicLong totSec;
     private volatile boolean stop;
     private final PlayerType playerType;
     private final GameView gameView;
 
     PlayerTimer(final GameView gameView, final PlayerType playerType) {
-        this.totSec = 0;
+        this.totSec = new AtomicLong(0L);
         stop = false;
         this.playerType = playerType;
         this.gameView = gameView;
@@ -19,19 +21,18 @@ public class PlayerTimer implements Runnable {
 
     @Override
     public final void run() {
-        while (totSec >= 0 && !stop) {
+        while (this.totSec.get() >= 0 && !stop) {
             try {
-                this.gameView.updatePlayerTimer(this.totSec, this.playerType);
-                this.totSec++;
                 Thread.sleep(1000);
+                this.gameView.updatePlayerTimer((int) this.totSec.incrementAndGet(), this.playerType);
             } catch (InterruptedException ex) {
                 ex.printStackTrace();
             }
         }
     }
-    /** Reset the timer (restart from 00:00). */
+    /* Reset the timer (restart from 00:00). */
     public final void resetTimer() {
-        this.totSec = 0;
+        this.totSec.set(0L);
     }
 
     public final void stopTimer() {
